abfd4c96bf351b68cc396a44fc137896cae6f54f
cgad <35946778+cgad@users.noreply.github.com>
2018-09-17 13:22:59 -0600

    Front- and Back-end api routing, commented out text capture in index.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,5 @@
+// GIVEN STARTER CODE
+
 var db = require("../models");
 
 module.exports = function(app) {
@@ -22,3 +24,46 @@ module.exports = function(app) {
     });
   });
 };
+
+
+// DatumBox API NODE.JS CODE
+
+
+// api-key: 917ef13adf4f58b8421cc6a161f94f5a
+var datum = require("datumbox").factory("917ef13adf4f58b8421cc6a161f94f5a");
+
+
+// asynchronous parallel service request from DatumBox on text.
+//
+// list of service values:
+// 'SentimentAnalysis', 'TwitterSentimentAnalysis', 'SubjectivityAnalysis', 'TopicClassification', 'SpamDetection', 'AdultContentDetection', 'ReadabilityAssessment', 'LanguageDetection', 'CommercialDetection', 'EducationalDetection', 'GenderDetection', 'TextExtraction'
+//
+// example function
+//
+// datum.parallel(
+//   "So, do you dare to find different sentiments of me in one request?", 
+//   ['SentimentAnalysis', 'TwitterSentimentAnalysis', 'GenderDetection'], // array of service values
+//  function(err, results) {
+//      if ( err )
+//       return console.error(err);
+      
+//       // results in [] and have the output in same service format.
+//  }
+// );
+
+// textToCheck = string of user input in textarea
+// serviceValues = array of service value strings of user input in check boxes
+app.post("/api/results", function(err, res) {
+  
+  var serviceValues = ['SubjectivityAnalysis', 'SentimentAnalysis', 'TopicClassification', 'AdultContentDetection', ]
+  datum.parallel(textToCheck, serviceValues,
+    function(err, results) {
+      if (err) return console.error(err);
+      // return results array matching serviceValues array
+      return(results);
+    }
+  );
+})
+
+// export to server.js
+module.exports = datum;
